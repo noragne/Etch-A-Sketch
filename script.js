f7d9232ebceff3f2acd89ae99c8f7f59666301a9
Noragne <noragne@gmail.com>
2018-12-03 17:14:35 +0100

    Add setGrid Function

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ let container = document.querySelector("#container");
 let btnReset = document.querySelector("#reset");
 let btnSetGrid = document.querySelector("#setGrid");
 let pen = document.querySelector("#pen");
+
+
+// Function to create a grid with num width & num height
+function setGrid(num) {
+    // Set the grid
+    container.style.grid = `repeat(${num}, 1fr) / repeat(${num}, 1fr)`;
+
+    // create boxes in the grid
+    for (let i = 0; i < num ** 2; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);
+    }
+}
+
+
+
+
+
+
+
+
+
+// Populate the page
+setGrid(16);
