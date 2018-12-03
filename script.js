91a1db75cf1f83d088d182aee4e75e8342d4728f
Noragne <noragne@gmail.com>
2018-12-03 17:05:49 +0100

    Create script.js, linked it & Declare variable for DOM elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Variables for DOM elments
+let container = document.querySelector("#container");
+let btnReset = document.querySelector("#reset");
+let btnSetGrid = document.querySelector("#setGrid");
+let pen = document.querySelector("#pen");
